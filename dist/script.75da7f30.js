2faa8a763722ed3d58b8e3b7ba59674aae30cbe3
MFerguson1449 <mferguson1449@gmail.com>
2021-01-03 14:39:36 -0500

    mobible compatibility update

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -2036,7 +2036,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "20828" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3090" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
