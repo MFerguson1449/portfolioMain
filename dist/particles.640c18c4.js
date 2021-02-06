bb1ec4fedb013bc593bfa3132e66992629d2bad5
MFerguson1449 <mferguson1449@gmail.com>
2021-02-05 21:32:51 -0500

    updated experience cards

--- dist/particles.640c18c4.js
+++ dist/particles.640c18c4.js
@@ -1754,7 +1754,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6710" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8130" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
