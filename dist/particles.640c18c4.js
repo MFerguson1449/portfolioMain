31065d52738adbff5b051df3e178e623be9e33d8
MFerguson1449 <mferguson1449@gmail.com>
2021-04-15 19:50:31 -0400

    skills section refactor

--- dist/particles.640c18c4.js
+++ dist/particles.640c18c4.js
@@ -1754,7 +1754,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58876" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57704" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
